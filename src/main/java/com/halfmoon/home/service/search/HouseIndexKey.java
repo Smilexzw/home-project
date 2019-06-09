2562bce1a7357f9e53f1c2f5cbbd51b6718f6c05
xuzhangwang <1114998422@qq.com>
2019-06-09 20:11:23 +0800

    索引关键词定义

---
+++ src/main/java/com/halfmoon/home/service/search/HouseIndexKey.java
@@ -0,0 +1,48 @@
+package com.halfmoon.home.service.search;
+
+/**
+ * 索引关键词定义
+ * @author: xuzhangwang
+ */
+public class HouseIndexKey {
+
+    public static final String HOUSE_ID = "houseId";
+
+    public static final String TITLE = "title";
+
+    public static final String PRICE = "price";
+
+    public static final String AREA = "area";
+
+    public static final String CREATE_TIME = "createTime";
+
+    public static final String LAST_UPDATE_TIME = "lastUpdateTime";
+
+    public static final String CITY_EN_NAME = "cityEnName";
+
+    public static final String REGION_EN_NAME = "regionEnName";
+
+    public static final String DIRECTION = "direction";
+
+    public static final String DISTAANCE_TO_SUBWAY = "distanceTOSubway";
+
+    public static final String SUBWAYLINENAME = "subwayLineName";
+
+    public static final String SUBWAYSTATIONNAME = "subwayStationName";
+
+    public static final String TAGS = "tags";
+
+    public static final String STREET = "street";
+
+    public static final String DISTRICT = "district";
+
+    public static final String DESCRIPTION = "description";
+
+    public static final String LAYOUTDESC = "layoutDesc";
+
+    public static final String TRAFFIC = "traffic";
+
+    public static final String REOUNDSERVICE = "roundService";
+
+    public static final String RENTWAY = "rentWay";
+}
