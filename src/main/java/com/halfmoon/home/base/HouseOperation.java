a21b513f922924448641c42314a1fb70b6d5b210
xuzhangwang <1114998422@qq.com>
2019-06-09 20:04:21 +0800

    房屋操作状态常量定义

---
+++ src/main/java/com/halfmoon/home/base/HouseOperation.java
@@ -0,0 +1,28 @@
+package com.halfmoon.home.base;
+
+/**
+ * 房屋操作状态常量定义
+ * @author: xuzhangwang
+ */
+public class HouseOperation {
+
+    /**
+     * 发布
+     */
+    public static final int PASS = 1;
+
+    /**
+     * 下架
+     */
+    public static final int Obtained = 2;
+
+    /**
+     * 删除
+     */
+    public static final int DELETE = 3;
+
+    /**
+     * 房屋出租
+     */
+    public static final int RENT = 4;
+}
